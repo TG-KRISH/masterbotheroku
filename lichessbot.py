13c2467c9acf9ea80d9518958b10633dd34e948c
Gokul <79579584+TG-KRISH@users.noreply.github.com>
2021-04-04 20:50:53 +0530

    Update lichessbot.py

--- lichessbot.py
+++ lichessbot.py
@@ -115,8 +115,8 @@ ponder_results = {}
 
 @backoff.on_exception(backoff.expo, BaseException, max_time=600, giveup=is_final)
 def play_game(li, game_id, engine_factory, user_profile, config):
-    li.chat(game_id,"player","Hello buddy! I am @Unkown_2009 This bot has been made by @Entcingseal And @master_bot. NEED A BOT? contact @master_bot :D ")
-    li.chat(game_id,"spectator","Hi Guys!  I am @Unkown_2009 This bot has been made by @Entcingseal And @master_bot. NEED A BOT? contact @master_bot XD ")
+    li.chat(game_id,"player","Hello buddy! I am @B0t_N0_1 This bot has been made by @Entcingseal. NEED A BOT? contact https://lichess.org/inbox/Enticingseal :D ")
+    li.chat(game_id,"spectator","Hi Guys!  I am @B0t_N0_1 This bot has been made by @Entcingseal. NEED A BOT? contact https://lichess.org/inbox/Enticingseal XD ")
     response = li.get_game_stream(game_id)
     lines = response.iter_lines()
     bullet=False
